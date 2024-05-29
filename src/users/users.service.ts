6544171272c0d23c4531660f8b35a798b0e40133
Aleksandr <dudkov_aleksandr@mail.ru>
2024-05-29 16:33:20 +0300

    f

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -16,7 +16,7 @@ export class UsersService {
         const user = await this.userRepository.create(dto);
         const role = await this.roleService.getRoleByValue("ADMIN")
         await user.$set('roles', [role.id])
-        user.roles = [role]
+        user.roles = role
         return user;
     }
 
